bfa6502d1bf156d2443bfbf42402007c59d3a8f6
ZAC-IA <projetoarcoiris76@gmail.com>
2026-09-02 12:20:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,36 +4,30 @@ const error = document.getElementById("error");
 const showPassword = document.getElementById("showPassword");
 
 showPassword.addEventListener("click", () => {
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    showPassword.textContent = "◉";
-  } else {
-    passwordInput.type = "password";
-    showPassword.textContent = "◉";
-  }
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPassword.textContent = "🙈";
+    } else {
+        passwordInput.type = "password";
+        showPassword.textContent = "👁️";
+    }
 });
 
 loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const senha = passwordInput.value;
+    const senha = passwordInput.value;
 
-  /*
-    IMPORTANTE:
-    Coloque aqui a senha que você escolheu.
-    Esta versão é apenas para o protótipo do GitHub Pages.
-  */
- const SENHA_CORRETA = "KINGGAMER123";
+    // SENHA DO SITE
+    const SENHA_CORRETA = "NEWULTRA13";
 
-  if (senha === SENHA_CORRETA) {
-    error.textContent = "";
+    if (senha === SENHA_CORRETA) {
+        error.textContent = "";
 
-    // Esconde o login
-    document.getElementById("login").classList.add("hidden");
+        document.getElementById("login").classList.add("hidden");
+        document.getElementById("app").classList.remove("hidden");
 
-    // Mostra o aplicativo
-    document.getElementById("app").classList.remove("hidden");
-  } else {
-    error.textContent = "❌ Senha incorreta!";
-  }
+    } else {
+        error.textContent = "❌ Senha incorreta!";
+    }
 });
